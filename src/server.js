6cda46f19b94ee7d443b51ce99c88d927414f8af
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2023-10-31 22:23:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,27 +19,22 @@ app.get("/users", (req, res) => {
   });
 });
 
-app.post("/form", (req, res) => {
+app.post("/users/create", (req, res) => {
   const { email, password } = req.body.user;
   const data = { email, password };
   try {
     fs.readFile("./db/users.json", "utf8", (err, fileData) => {
       if (err) {
-        console.error(err);
-        return;
+        res.status(400).json({ message: "error!" });
       }
 
       let users = [];
-      if (fileData) {
-        users = JSON.parse(fileData);
-      }
-
+      users = JSON.parse(fileData);
       users.push(data);
 
       fs.writeFile("./db/users.json", JSON.stringify(users, null, 1), (err) => {
         if (err) {
-          console.error(err);
-          return;
+          return res.status(400).json({ message: "error!" });
         }
         res.status(201).json({ message: "created" });
       });
@@ -49,7 +44,7 @@ app.post("/form", (req, res) => {
   }
 });
 
-app.delete("/users/exclude", (req, res) => {
+app.delete("/users/delete", (req, res) => {
   const { email } = req.body;
 
   fs.readFile("./db/users.json", "utf8", (err, data) => {
@@ -59,7 +54,10 @@ app.delete("/users/exclude", (req, res) => {
 
     if (data) {
       const users = JSON.parse(data);
-      const userPosition = users.findIndex((user) => user.email == email);
+      const userPosition = users.findIndex((user) => user.email === email);
+      if (users[userPosition] == undefined)
+        return res.status(400).json({ message: "Essa conta não existe!" });
+
       users.splice(userPosition);
 
       const newUsersList = users;
@@ -70,10 +68,41 @@ app.delete("/users/exclude", (req, res) => {
         (err) => {
           if (err) throw err;
 
-          res.status(200).json({ message: "excluded" });
+          res.status(200).json({ message: "deleted!" });
         }
       );
     }
+  });
+});
+
+app.put("/users/update", (req, res) => {
+  const { password, email, newPassword } = req.body;
+
+  fs.readFile("./db/users.json", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(200).json({ message: "error!" });
+    }
+
+    const users = JSON.parse(data);
+    const user = users.find((user) => {
+      if(user.email === email) return user
+    });
+    if (user.password !== password) {
+      return res.status(200).json({ message: "senha incorreta!" });
+    }
+    users[userPosition].password = newPassword;
+    console.log(users[userPosition].password)
+
+    const newUsersList = users;
+
+    fs.writeFile(
+      "./db/users.js",
+      JSON.stringify(newUsersList, null, 1),
+      (err) => {
+        if (err) throw err;
+        res.status(200).json({ message: "updated!" });
+      }
+    );
   });
 });
 
